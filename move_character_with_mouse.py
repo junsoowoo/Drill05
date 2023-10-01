0fe45230b4819bd3a9193b5821ab44a7ac8ab754
Junsoo Han <hanjs991124@gmail.com>
2023-10-01 18:33:50 +0900

    함수 내용추가

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -7,48 +7,41 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-hand=load_image('hand_arrow.png')
+cursor = load_image('hand_arrow.png')
 
-def moving_character():
+def moving_character(character_x, character_y, cursor_x, cursor_y):
+    dir_x = cursor_x - character_x
+    dir_y = cursor_y - character_y
+    move_speed = 5
+    distance = math.sqrt(dir_x ** 2 + dir_y ** 2)
+    if distance > move_speed:
+        dir_x /= distance
+        dir_y /= distance
+        character_x += dir_x * move_speed
+        character_y += dir_y * move_speed
 
-    pass
+    return character_x, character_y
 
 def random_xy():
-
-    pass
-
-def handle_events():
-    global running
-    global x, y
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-    pass
-
-
+    return random.randint(0, TUK_WIDTH - 1), random.randint(0, TUK_HEIGHT - 1)
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+cursor_x, cursor_y = random_xy()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    random_xy()
-    moving_character()
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    cursor.draw(cursor_x, cursor_y)
+    character_x, character_y = moving_character(character_x, character_y, cursor_x, cursor_y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
     update_canvas()
     frame = (frame + 1) % 8
-
-    handle_events()
+    distance_to_target = math.sqrt((character_x - cursor_x) ** 2 + (character_y - cursor_y) ** 2)
+    if distance_to_target < 5:
+        cursor_x, cursor_y = random_xy()
+    delay(0.009)
 
 close_canvas()
-
-
-
-
