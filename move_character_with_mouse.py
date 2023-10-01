a1a09c9401a70258eb6bd11206cd3938a41dc754
Junsoo Han <hanjs991124@gmail.com>
2023-10-01 18:43:19 +0900

    캐릭터 방향설정

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -20,7 +20,7 @@ def moving_character(character_x, character_y, cursor_x, cursor_y):
         character_x += dir_x * move_speed
         character_y += dir_y * move_speed
 
-    return character_x, character_y
+    return character_x, character_y, dir_x, dir_y
 
 def random_xy():
     return random.randint(0, TUK_WIDTH - 1), random.randint(0, TUK_HEIGHT - 1)
@@ -30,13 +30,19 @@ x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 cursor_x, cursor_y = random_xy()
+character_direction = 0
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     cursor.draw(cursor_x, cursor_y)
-    character_x, character_y = moving_character(character_x, character_y, cursor_x, cursor_y)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
+    character_x, character_y, dir_x, dir_y = moving_character(character_x, character_y, cursor_x, cursor_y)
+    if dir_x > 0:
+        character_direction = 1
+    else:
+        character_direction = 0
+    sprite_row = 100 * character_direction
+    character.clip_draw(frame * 100, sprite_row, 100, 100, character_x, character_y)
     update_canvas()
     frame = (frame + 1) % 8
     distance_to_target = math.sqrt((character_x - cursor_x) ** 2 + (character_y - cursor_y) ** 2)
